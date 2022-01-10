ad0ac8ccbd62511eb2b12753632ba51660793572
namnguyen <hoainamnguyen2001@gmail.com>
2022-01-11 02:39:57 +0700

    second commit

--- backend/myshop/shopapp/views.py
+++ backend/myshop/shopapp/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
-from rest_framework import viewsets,status,generics
+from rest_framework import viewsets
 from .models import Product
 from .serializer import ProductSerializer
 
-def hello(request):
-    return HttpResponse("hello")
-
-class ProductView(viewsets.ViewSet, generics.ListAPIView):
+class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.filter(active = True)
     serializer_class = ProductSerializer
 
